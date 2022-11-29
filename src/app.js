b0074cbfe0104bb8660af0279188d8863602b405
SvitlanaStan <118539952+SvitlanaStan@users.noreply.github.com>
2022-11-29 18:52:58 +0200

    API 1

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
 }
-let apiKey = "140b3eb45f31cd8abo75dta8374d8f50";
-let query = "New York";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}&units=metric`;
+
+// let apiKey = "140b3eb45f31cd8abo75dta8374d8f50";
+// let query = "Lisbon";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=140b3eb45f31cd8abo75dta8374d8f50&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
