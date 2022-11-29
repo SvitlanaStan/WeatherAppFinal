7829d2c8a25c4db17c56f22f2483aa8cf8b6b9df
SvitlanaStan <118539952+SvitlanaStan@users.noreply.github.com>
2022-11-29 18:27:50 +0200

    API start

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+let apiKey = "140b3eb45f31cd8abo75dta8374d8f50";
+let query = "New York";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}&units=metric`;
 
+axios.get(apiUrl).then(displayTemperature);
