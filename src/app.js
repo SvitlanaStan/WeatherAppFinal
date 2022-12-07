e37e8eb84c060355ffd515c400f0529d3a7597b4
SvitlanaStan <118539952+SvitlanaStan@users.noreply.github.com>
2022-12-07 15:07:47 +0200

    Update

--- src/app.js
+++ src/app.js
@@ -38,8 +38,6 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-// let apiKey = "140b3eb45f31cd8abo75dta8374d8f50";
-// let query = "Lisbon";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=140b3eb45f31cd8abo75dta8374d8f50&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
