a37fe72c1b7834678c15f1229aa842b2b87a645e
SvitlanaStan <118539952+SvitlanaStan@users.noreply.github.com>
2022-12-09 14:35:16 +0200

    Forecast Function Added

--- src/app.js
+++ src/app.js
@@ -21,9 +21,33 @@ function formatDate() {
 
   return `${day} ${hours}:${minutes}`;
 }
+
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = "Forecast";
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 10° </span>
+          <span class="weather-forecast-temperature-min"> 7° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
 }
 
 function displayTemperature(response) {
@@ -90,4 +114,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
 displayForecast();
